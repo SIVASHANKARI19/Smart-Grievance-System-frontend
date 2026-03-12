50c08b62a96d6da1239f98e28a08d01761588b7d
SIVASHANKARI19 <151622873+SIVASHANKARI19@users.noreply.github.com>
2026-03-12 09:07:11 +0530

    added auth for register

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import api from './api';
 
 export const authService = {
-  register: async (userData) => {
+  register : async (userData) => {
     const response = await api.post('/auth/register', userData);
     return response.data;
   },
