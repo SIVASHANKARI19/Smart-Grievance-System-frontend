df09dc80722e6c9ca69a5da761b530c44821694f
SIVASHANKARI19 <151622873+SIVASHANKARI19@users.noreply.github.com>
2026-03-19 08:57:48 +0530

    api

--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,13 @@
-import axios from 'axios';
+
+ import axios from 'axios';
+
 
 const api = axios.create({
   baseURL: 'http://localhost:5000/api',
 });
 
 // Add a request interceptor to include the JWT token
+
 api.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem('token');
