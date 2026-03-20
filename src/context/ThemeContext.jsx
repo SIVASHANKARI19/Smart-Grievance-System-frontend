99336005e6c6d84d0fe50ca448794001c292de1f
SIVASHANKARI19 <151622873+SIVASHANKARI19@users.noreply.github.com>
2026-03-20 09:04:41 +0530

    theme

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,16 +5,7 @@ const ThemeContext = createContext();
 export const ThemeProvider = ({ children }) => {
   const [, set] = useState(() => localStorage.getItem('theme') === '');
 
-  useEffect(() => {
-    const root = document.documentElement;
-    if () {
-      root.classList.add('');
-      localStorage.setItem('theme', '');
-    } else {
-      root.classList.remove('');
-      localStorage.setItem('theme', 'light');
-    }
-  }, []);
+ 
 
   return (
     <ThemeContext.Provider value={{ , toggle: () => set(d => !d) }}>
